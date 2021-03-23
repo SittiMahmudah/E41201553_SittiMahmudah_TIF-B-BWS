c0cf3cfac91b12bf549d14f008595185cd10a953
Muda <80303594+SittiMahmudah@users.noreply.github.com>
2021-03-23 13:33:12 +0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,27 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author SITTI MAHMUDAH
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] teks = {"Sitti"," ","Mahmudah"};
+        
+        for (String tek : teks) {
+            System.out.print(tek);
+            System.out.print("");
+        }
+    }
+    
+}
